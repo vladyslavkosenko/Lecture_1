c6c847f94e680354c0900808efd28dd54179de93
vladyslavkosenko <vaduha91@gmail.com>
2021-12-21 02:43:26 +0200

    wrote down arithmetic calculations

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x = 1, y = 2, a = 3, w = 4, b = 5, X = 6, e = 7;
+
+        double result1, result2, result3, result4;
+        result1 = x + y - a / 2 * (x + w);
+        result2 = a * (x + x) / y - a * 3;
+        result3 = a + b / x - y;
+        result4 = X - e / 2 % e + 1;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
     }
 }
